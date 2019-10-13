cc8d484419d22832449352d39eb98abd2d000ff3
zhangsen <zhangsen839705693@163.com>
2019-10-14 00:20:41 +0800

    修改ui

--- app/src/main/java/ccash/android/com/ui/fragment/AddMakeStyleFragment.java
+++ app/src/main/java/ccash/android/com/ui/fragment/AddMakeStyleFragment.java
@@ -2,6 +2,7 @@ package ccash.android.com.ui.fragment;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.FrameLayout;
@@ -11,46 +12,42 @@ import android.widget.TextView;
 import com.growalong.util.util.GALogger;
 import com.lxj.xpopup.XPopup;
 import com.lxj.xpopup.interfaces.OnConfirmListener;
+import com.lxj.xpopup.interfaces.OnInputConfirmListener;
 import com.lxj.xpopup.interfaces.XPopupCallback;
 import butterknife.BindView;
 import butterknife.OnClick;
 import ccash.android.com.BaseFragment;
 import ccash.android.com.R;
 import ccash.android.com.app.AccountManager;
+import ccash.android.com.presenter.CenterPresenter;
+import ccash.android.com.presenter.contract.CenterContract;
+import ccash.android.com.presenter.modle.CenterModle;
 import ccash.android.com.ui.activity.AddMakeStyleActivity;
 import ccash.android.com.ui.activity.AliPayListActivity;
 import ccash.android.com.ui.activity.IdCastPayListActivity;
 import ccash.android.com.ui.activity.WebChatListActivity;
+import ccash.android.com.util.ToastUtil;
 
-public class AddMakeStyleFragment extends BaseFragment {
+public class AddMakeStyleFragment extends BaseFragment implements CenterContract.View {
     private static final String TAG = AddMakeStyleFragment.class.getSimpleName();
     @BindView(R.id.iv_back)
     ImageView ivBack;
     @BindView(R.id.tv_title)
     TextView tvTitle;
-    @BindView(R.id.tv_alipay)
-    TextView tvAlipay;
     @BindView(R.id.ll_alipay_click)
     LinearLayout llAlipayClick;
-    @BindView(R.id.tv_ylcard)
-    TextView tvYlcard;
     @BindView(R.id.ll_ylcard_click)
     LinearLayout llYlcardClick;
-    @BindView(R.id.tv_webchat)
-    TextView tvWebchat;
     @BindView(R.id.ll_webchat_click)
     LinearLayout llWebchatClick;
     @BindView(R.id.fl_title_comtent)
     FrameLayout flTitleComtent;
-//    @BindView(R.id.tv_yunshanfu)
-//    TextView tvYunshanfu;
-//    @BindView(R.id.ll_yunshanfu_click)
-//    LinearLayout llYunshanfuClick;
-//    @BindView(R.id.tv_lacara)
-//    TextView tvLacara;
-//    @BindView(R.id.ll_lacara_click)
-//    LinearLayout llLacaraClick;
+    @BindView(R.id.tv_nickname)
+    TextView tvNickname;
+    @BindView(R.id.ll_nickname_click)
+    LinearLayout llNicknameClick;
     private AddMakeStyleActivity addMakeStyleActivity;
+    private CenterPresenter presenter;
 
     public static AddMakeStyleFragment newInstance(@Nullable String taskId) {
         Bundle arguments = new Bundle();
@@ -79,9 +76,11 @@ public class AddMakeStyleFragment extends BaseFragment {
     public void lazyLoadData() {
         super.lazyLoadData();
         GALogger.d(TAG, "lazyLoadData  ........");
+        //初始化presenter
+        new CenterPresenter(this, new CenterModle());
     }
 
-    @OnClick({R.id.iv_back, R.id.ll_alipay_click, R.id.ll_ylcard_click, R.id.ll_webchat_click})
+    @OnClick({R.id.iv_back, R.id.ll_alipay_click, R.id.ll_ylcard_click, R.id.ll_webchat_click,R.id.ll_nickname_click})
     public void onViewClicked(View view) {
         switch (view.getId()) {
             case R.id.iv_back:
@@ -117,79 +116,61 @@ public class AddMakeStyleFragment extends BaseFragment {
                                 }
                             }, null, false)
                             .show();
-                }else {
+                } else {
                     IdCastPayListActivity.startThis(addMakeStyleActivity);
                 }
                 break;
             case R.id.ll_webchat_click:
                 WebChatListActivity.startThis(addMakeStyleActivity);
                 break;
-//            case R.id.ll_yunshanfu_click:
-//                YunShanFuListActivity.startThis(addMakeStyleActivity);
-//                break;
-//            case R.id.ll_lacara_click:
-//                if (!AccountManager.getInstance().isHaveWechatPayee()) {
-//                        //带确认和取消按钮的弹窗
-//                    new XPopup.Builder(getContext())
-//                        //                         .dismissOnTouchOutside(false)
-//                            // 设置弹窗显示和隐藏的回调监听
-//                        //                         .autoDismiss(false)
-//                        //                        .popupAnimation(PopupAnimation.NoAnimation)
-//                            .setPopupCallback(new XPopupCallback() {
-//                                @Override
-//                                public void onShow() {
-//                                    Log.e("tag", "onShow");
-//                                }
-//
-//                                @Override
-//                                public void onDismiss() {
-//                                    Log.e("tag", "onDismiss");
-//                                }
-//                            }).asConfirm("请先绑定微信收款方式", "",
-//                            "取消", "确定",
-//                            new OnConfirmListener() {
-//                                @Override
-//                                public void onConfirm() {
-//                                    WebChatListActivity.startThis(addMakeStyleActivity);
-//                                }
-//                            }, null, false)
-//                            .show();
-//                }else {
-//                    LaCaraListActivity.startThis(addMakeStyleActivity);
-//                }
-//                break;
+            case R.id.ll_nickname_click:
+                new XPopup.Builder(getContext())
+                        .dismissOnBackPressed(false)
+                        .dismissOnTouchOutside(false)
+                        .autoOpenSoftInput(true)
+//                        .moveUpToKeyboard(false) //是否移动到软键盘上面，默认为true
+                        .asInputConfirm("请输入要修改的昵称", "", "昵称", false,
+                                new OnInputConfirmListener() {
+                                    @Override
+                                    public void onConfirm(String text) {
+                                        if (!TextUtils.isEmpty(text)) {
+                                            presenter.changeNickname(text);
+                                        } else {
+                                            ToastUtil.shortShow("请输入要修改的昵称");
+                                        }
+                                    }
+                                })
+                        .show();
+                break;
         }
     }
 
     @Override
     public void onResume() {
         super.onResume();
-        tvTitle.setText("添加收款方式");
-        if (AccountManager.getInstance().isHaveAliPayee()) {
-            tvAlipay.setText("已绑定");
-        } else {
-            tvAlipay.setText("未绑定");
-        }
-        if (AccountManager.getInstance().isHaveBankPayee()) {
-            tvYlcard.setText("已绑定");
-        } else {
-            tvYlcard.setText("未绑定");
-        }
-        if (AccountManager.getInstance().isHaveWechatPayee()) {
-            tvWebchat.setText("已绑定");
-        } else {
-            tvWebchat.setText("未绑定");
-        }
-//        if (AccountManager.getInstance().isHaveCloudPayee()) {
-//            tvYunshanfu.setText("已绑定");
-//        } else {
-//            tvYunshanfu.setText("未绑定");
-//        }
-//        if (AccountManager.getInstance().isHaveLakalaPayee()) {
-//            tvLacara.setText("已绑定");
-//        } else {
-//            tvLacara.setText("未绑定");
-//        }
+        tvTitle.setText("法币设置");
+        tvNickname.setText(AccountManager.getInstance().getNickname());
         GALogger.d(TAG, "onResume  ........");
+    }
+
+    @Override
+    public void changeNicknameSuccess(String nickname) {
+        AccountManager.getInstance().setNickname(nickname);
+        tvNickname.setText(AccountManager.getInstance().getNickname());
+    }
+
+    @Override
+    public void setPresenter(CenterContract.Presenter presenter) {
+        this.presenter = (CenterPresenter) presenter;
+    }
+
+    @Override
+    public void showLoading() {
+        showLoadingDialog();
+    }
+
+    @Override
+    public void hideLoading() {
+        hideLoadingDialog();
     }
 }
